f6dc2390c0528ae7ed67d17aaa2d73c94885ed07
zhuguol <1556141833@qq.com>
2019-07-17 10:31:48 +0800

    20190717

--- 修改待办任务20190703/脚本.sql
+++ 修改待办任务20190703/脚本.sql
@@ -14,3 +14,10 @@ from
 )t2
 where snt > 0
 order by t2.work_Date;
+
+insert into SYS_PARAMS (PARAMGROUP_ID, PARAM_ID, PARAM_VAL, PARAM_NAME, MEMO, ST, IS_LOCK, IS_DEL, CRT_DT, LAST_UPD_TMS, LAST_UPD_OPER)
+values ('AML3', 'TODO_BH_ZC', '140_33,140_34', '主页大额交易自查', null, '4', 'F', 'F', null, null, null);
+insert into SYS_PARAMS (PARAMGROUP_ID, PARAM_ID, PARAM_VAL, PARAM_NAME, MEMO, ST, IS_LOCK, IS_DEL, CRT_DT, LAST_UPD_TMS, LAST_UPD_OPER)
+values ('AML3', 'TODO_BH', '30_30101,30_30102,30_30103', '主页大额代办任务', null, '4', 'F', 'F', null, null, null);
+insert into SYS_PARAMS (PARAMGROUP_ID, PARAM_ID, PARAM_VAL, PARAM_NAME, MEMO, ST, IS_LOCK, IS_DEL, CRT_DT, LAST_UPD_TMS, LAST_UPD_OPER)
+values ('AML3', 'TODO_BS_ZC', '140_31,140_32', '主页可疑交易自查', null, '4', 'F', 'F', null, null, null);
