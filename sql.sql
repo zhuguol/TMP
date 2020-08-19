e5867d8bd82fa8a14d951ed45197b5d9bc351f8a
zhuguol <1556141833@qq.com>
2020-08-19 16:12:56 +0800

    20200819

--- sql.sql
+++ sql.sql
@@ -1,86 +1,75 @@
 CREATE OR REPLACE VIEW VI_HOMEPAGE_TODO_CREDIT AS
-SELECT SYS_GUID() as id,T.ALARM_DATE,T.OPERATOR_TLR,T.RISK_LEVEL,T.FILED1,T.FILED2 FROM(select m.ALARM_DATE,
-       m.OPERATOR_TLR,
-       m.RISK_LEVEL,
-       sum(case
-             when m.MESSAGE_RESULT in ('02','04') and
-                  (to_date(substr(m.operator_time1,1,8),'yyyymmdd') <=
-                  (select c.natural_date
-                      from (select rownum as rn, b.natural_date
-                              from (select b.natural_date
-                                      from aml_sshldyp b
-                                     where b.natural_date < sysDate
-                                       and b.is_holiday = '0'
-                                     order by b.natural_date desc) b) c
-                     where c.rn = 5)) then
-              1
-             else
-              0
-           end) as filed1,
-       sum(case
-             when (m.OUTBOUND_RESULT = '06' or m.OUTBOUND_RESULT is null) and
-                  (to_date(substr(m.operator_time2,1,8),'yyyymmdd') <=
-                  (select c.natural_date
-                      from (select rownum as rn, b.natural_date
-                              from (select b.natural_date
-                                      from aml_sshldyp b
-                                     where b.natural_date < sysDate
-                                       and b.is_holiday = '0'
-                                     order by b.natural_date desc) b) c
-                     where c.rn = 1)) then
-              1
-             else
-              0
-           end) as filed2
-  from (SELECT a.ALARM_DATE,
+SELECT SYS_GUID() as id,
+       T.ALARM_DATE,
+       T.OPERATOR_TLR,
+       T.RISK_LEVEL,
+       T.FILED1,
+       T.FILED2
+  FROM (SELECT a.ALARM_DATE,
                a.OPERATOR_TLR,
                a.RISK_LEVEL,
-               a.MESSAGE_RESULT,
-               a.OUTBOUND_RESULT,
-               f.alarm_no,
-               f.operator_time as operator_time1,
-               k.alarm_no,
-               k.operator_time as operator_time2
+               sum(case
+                     when a.MESSAGE_RESULT in ('02', '04') and
+                          (to_date(substr(g.operator_time, 1, 8), 'yyyymmdd') <=
+                          (select c.natural_date
+                              from (select rownum as rn, b.natural_date
+                                      from (select b.natural_date
+                                              from aml_sshldyp b
+                                             where b.natural_date <= sysDate
+                                               and b.is_holiday = '0'
+                                             order by b.natural_date desc) b) c
+                             where c.rn = 6)) then
+                      1
+                     else
+                      0
+                   end) as filed1,
+               sum(case
+                     when (a.OUTBOUND_RESULT = '06' or a.OUTBOUND_RESULT is null) and
+                          (to_date(substr(h.operator_time, 1, 8), 'yyyymmdd') <=
+                          (select c.natural_date
+                              from (select rownum as rn, b.natural_date
+                                      from (select b.natural_date
+                                              from aml_sshldyp b
+                                             where b.natural_date <= sysDate
+                                               and b.is_holiday = '0'
+                                             order by b.natural_date desc) b) c
+                             where c.rn = 2)) then
+                      1
+                     else
+                      0
+                   end) as filed2
           from CREDIT_CARD_ALERT a
-          left join (select e.alarm_no, e.operator_time
-                      from (select d.alarm_no,
-                                   d.operator_time,
-                                   row_number() over(partition by d.alarm_no order by d.operator_time desc) as rn
-                              from (select c.alarm_no, c.operator_time
-                                      from credit_card_audit_trail c
-                                     where c.fileld_name = '短信结果'
-                                       and c.alarm_no =
-                                           (select b.alarm_no
-                                              from CREDIT_CARD_ALERT b
-                                             where b.INVESTIGATE_RESULT = '01'
-                                               and b.MESSAGE_RESULT in
-                                                   ('02', '04'))) d) e
-                     where e.rn = '1') f on f.alarm_no = a.alarm_no
-          left join (select i.alarm_no, i.operator_time
-                      from (select h.alarm_no,
-                                   h.operator_time,
-                                   row_number() over(partition by h.alarm_no order by h.operator_time desc) rn
-                              from (select j.alarm_no, j.operator_time
-                                      from credit_card_audit_trail j
-                                     where j.fileld_name = '外呼结果'
-                                       and j.alarm_no =
-                                           (select g.alarm_no
-                                              from CREDIT_CARD_ALERT g
-                                             where g.INVESTIGATE_RESULT = '01'
-                                               and (g.OUTBOUND_RESULT = '06' or
-                                                   g.OUTBOUND_RESULT is null))) h) i
-                     where i.rn = '1') k on k.alarm_no = a.alarm_no
+          left join (select f.alarm_no, f.operator_time
+                      from (select e.alarm_no,
+                                   e.operator_time,
+                                   row_number() over(partition by e.alarm_no order by e.operator_time desc) rn
+                              from (select d.alarm_no, d.operator_time
+                                      from credit_card_audit_trail d
+                                     where d.fileld_name = '短信结果'
+                                       and d.alarm_no =
+                                           (select k.alarm_no
+                                              from CREDIT_CARD_ALERT k
+                                             where k.INVESTIGATE_RESULT = '01'
+                                               and k.MESSAGE_RESULT in
+                                                   ('02', '04'))) e) f
+                     where f.rn = '1') g on a.alarm_no = g.alarm_no
+          left join (select f.alarm_no, f.operator_time
+                      from (select e.alarm_no,
+                                   e.operator_time,
+                                   row_number() over(partition by e.alarm_no order by e.operator_time desc) rn
+                              from (select d.alarm_no, d.operator_time
+                                      from credit_card_audit_trail d
+                                     where d.fileld_name = '外呼结果'
+                                       and d.alarm_no =
+                                           (select l.alarm_no
+                                              from CREDIT_CARD_ALERT l
+                                             where l.INVESTIGATE_RESULT = '01'
+                                               and (l.OUTBOUND_RESULT = '06' or
+                                                   l.OUTBOUND_RESULT is null))) e) f
+                     where f.rn = '1') h on a.alarm_no = h.alarm_no
          where a.INVESTIGATE_RESULT = '01'
            and (a.MESSAGE_RESULT in ('02', '04') or
                (a.OUTBOUND_RESULT = '06' or a.OUTBOUND_RESULT is null))
-         group by a.ALARM_DATE,
-                  a.ALARM_DATE,
-                  a.OPERATOR_TLR,
-                  a.RISK_LEVEL,
-                  a.MESSAGE_RESULT,
-                  a.OUTBOUND_RESULT,
-                  f.alarm_no,
-                  f.operator_time,
-                  k.alarm_no,
-                  k.operator_time) m
- group by m.ALARM_DATE, m.OPERATOR_TLR, m.RISK_LEVEL)T;
+         group by a.ALARM_DATE, a.OPERATOR_TLR, a.RISK_LEVEL) T
+ WHERE (T.FILED1 <> 0 OR T.FILED2 <> 0)
+ ORDER BY T.ALARM_DATE DESC;
